044c86ffae4dfa5ff34543e185d1db60a5755d74
gokulchandragoshmkg <135825890+gokulchandragoshmkg@users.noreply.github.com>
2024-01-04 17:13:59 +0530

    modified

---
+++ src/components/sidebar.js
@@ -0,0 +1,28 @@
+function Sidebar() {
+    return(
+        <div className='d-flex flex-column flex-shrink-0 p-3 bg-light'>
+    <ul className='nav nav-pills flex-column mb-auto'>
+      <li className='nav-item'> 
+      <a href="#" className="nav-link active" aria-current="page">
+          <svg className="bi me-2" width="16" height="16"></svg>
+          Home
+        </a>
+      </li>
+      <li className='nav-item'> 
+      <a href="#" className="nav-link active" aria-current="page">
+          <svg className="bi me-2" width="16" height="16"></svg>
+          Home
+        </a>
+      </li>
+      <li className='nav-item'> 
+      <a href="#" className="nav-link active" aria-current="page">
+          <svg className="bi me-2" width="16" height="16"></svg>
+          Home
+        </a>
+      </li>
+    </ul></div>
+    )
+    
+}
+
+export default Sidebar;
