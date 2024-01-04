1c5a6b060774485c4432ca33d7fe4d7becf919c9
gokulchandragoshmkg <135825890+gokulchandragoshmkg@users.noreply.github.com>
2024-01-04 19:16:51 +0530

    ui added

---
+++ src/components/topbar.js
@@ -0,0 +1,15 @@
+function TopBar() {
+    return(<>
+        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">JVPG</a>
+      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"></input>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#">All access</a>
+        </li>
+      </ul>
+    </nav>
+    </>)
+}
+
+export default TopBar
